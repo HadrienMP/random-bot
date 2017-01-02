95a833506f7ad1d26b999ea7053078f411e680fe
HadrienMP <public@hadrienmp.fr>
2017-01-02 22:41:51 +0100

    Second commit... lazy committing

--- application/bot.py
+++ application/bot.py
@@ -1,23 +1,36 @@
 class Bot:
     def __init__(self):
         self.commands = dict()
-    
+
     def command(self, command):
-        
         def decorator(function):
             self.commands[command] = function
             return function
-            
+
         return decorator
-        
-    def respond_to(self, message):
-        if "item" in message.keys():
-        
-            command = message['item']['message']['message'].split(" ")[1]
-            command_function = self.commands.get(command)
+
+    def respond_to(self, json):
+        try:
+            command = Command(json)
+            command_function = self.commands.get(command.name)
             if command_function:
-                return command_function()
+                return command_function(command.parameters)
             else:
                 return "Oops I don't know this command :-("
-        else:
+        except ValueError:
             return "Oops I don't understand this message, maybe hipchat changed it's api ? :'("
+
+
+class Command:
+    def __init__(self, json):
+        if self.__is_well_formed(json):
+            message = json['item']['message']['message']
+            message_parts = message.split(" ")
+            self.name = message_parts[1]
+            self.parameters = message_parts[2] if len(message_parts) > 2 else ""
+        else:
+            raise ValueError("unkown message format")
+
+    @staticmethod
+    def __is_well_formed(json):
+        return "item" in json and "message" in json["item"] and "message" in json["item"]["message"]
--- test/bot_test.py
+++ test/bot_test.py
@@ -1,44 +1,66 @@
 from application.bot import Bot
 from assertpy import assert_that
 from test.utils import build_message
+from nose_parameterized import parameterized
+from ast import literal_eval
+from mock import patch
 
 bot = Bot()
 
+expected_response = "Je suis la commande test "
+
+
+@bot.command("command")
+def command_function(parameter=""):
+    return expected_response
+
+
 def test_should_call_the_mapped_function_for_a_mapped_command():
     # GIVEN
-    expected_response = "Je suis la commande test"
-    
-    @bot.command("test")
-    def test():
-        return expected_response
-        
-    message = build_message("/r test")
-        
+    message = build_message("/r command")
+
     # WHEN
     response = bot.respond_to(message)
-        
+
     # THEN
     assert_that(response).is_equal_to(expected_response)
-    
+
+
+@patch("test.bot_test.command_function")
+def test_should_call_the_mapped_function_for_a_mapped_command_with_the_parameter(command_function_mock):
+    # GIVEN
+    bot.command("command")(command_function_mock)
+    message = build_message("/r command nameless-param")
+
+    # WHEN
+    response = bot.respond_to(message)
+
+    # THEN
+    command_function_mock.assert_called_once_with("nameless-param")
+
 
 def test_should_return_a_static_error_message_for_an_unmapped_command():
     # GIVEN
     message = build_message("/r toto")
-        
+
     # WHEN
     response = bot.respond_to(message)
-        
+
     # THEN
     assert_that(response).is_equal_to("Oops I don't know this command :-(")
-    
-    
-def test_should_return_a_static_error_message_for_a_wrong_format_message():
-    # GIVEN
-    message = {'broken': 'message format'}
-        
+
+
+@parameterized([
+    "{'broken': 'message_format'}",
+    "{'item': 'still broken'}",
+    "{'item': {'message': 'still broken'}}",
+    "{'item': {'message': {'still': 'broken'}}}",
+])
+def test_should_return_a_static_error_message_for_a_wrong_format_message(message_str):
+    message = literal_eval(message_str)
+
     # WHEN
     response = bot.respond_to(message)
-        
+
     # THEN
     assert_that(response).is_equal_to("Oops I don't understand this message, maybe hipchat changed it's api ? :'(")
-    
