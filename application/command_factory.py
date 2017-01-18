bba3ff333525de57e429f5a5db7c29c7030b0c70
HadrienMP <public@hadrienmp.fr>
2017-01-18 21:30:05 +0100

    Handle impossible command names

--- application/command_factory.py
+++ application/command_factory.py
@@ -9,14 +9,10 @@ class CommandFactory:
         self.command_definitions = dict()
 
     def register(self, function, command_name, parameters):
-        if CommandFactory.__is_legal(command_name):
+        if _is_legal(command_name):
             self.command_definitions[command_name] = CommandDefinition(function, parameters)
         else:
             raise CommandNameError
-
-    @staticmethod
-    def __is_legal(command_name):
-        return re.match(r'^[^\s]+$', command_name)
 
     def create_for(self, json):
         try:
@@ -30,35 +26,39 @@ class CommandFactory:
             return StaticResponse("Oops, I don't understand what you are saying :'(")
 
 
+def _is_legal(command_name):
+    return re.match(r'^[^\s]+$', command_name)
+
+
 class CommandDefinition:
     def __init__(self, function, parameters):
         self.function = function
-        parameters += CommandDefinition.__detect_function_parameters(function)
-        self.parameters_mapping = CommandDefinition.__build_mapping(parameters)
+        parameters += _detect_function_parameters(function)
+        self.parameters_mapping = _build_mapping(parameters)
 
-    @staticmethod
-    def __detect_function_parameters(function):
-        function_parameters = [param.name for param in inspect.signature(function).parameters.values()]
-        function_parameters = [param for param in function_parameters if param not in ['args', 'kwargs']]
-        return function_parameters
 
-    @staticmethod
-    def __build_mapping(parameters) -> dict:
-        parameters_mapping = dict()
-        for parameter in parameters:
-            if "->" in parameter:
-                source_name, destination_name = parameter.split("->")
-                CommandDefinition.checkParameterName(destination_name)
-                parameters_mapping[source_name] = destination_name
-            else:
-                CommandDefinition.checkParameterName(parameter)
-                parameters_mapping[parameter] = parameter
-        return parameters_mapping
+def _detect_function_parameters(function):
+    function_parameters = [param.name for param in inspect.signature(function).parameters.values()]
+    function_parameters = [param for param in function_parameters if param not in ['args', 'kwargs']]
+    return function_parameters
 
-    @staticmethod
-    def checkParameterName(destination_name):
-        if not str(destination_name).isidentifier():
-            raise ParameterNameError
+
+def _build_mapping(parameters) -> dict:
+    parameters_mapping = dict()
+    for parameter in parameters:
+        if "->" in parameter:
+            source_name, destination_name = parameter.split("->")
+            __checkParameterName(destination_name)
+            parameters_mapping[source_name] = destination_name
+        else:
+            __checkParameterName(parameter)
+            parameters_mapping[parameter] = parameter
+    return parameters_mapping
+
+
+def __checkParameterName(destination_name):
+    if not str(destination_name).isidentifier():
+        raise ParameterNameError
 
 
 class ParameterNameError(Exception):
